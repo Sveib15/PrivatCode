fce80c79beb1bf4f487bf130ac3526dd4bd505d8
sveib15 <sveinbruce132@gmail.com>
2017-01-29 15:24:22 +0100

    Added the first .py file to the repo. waiting to learn about conditions next. this will increase the interaction with the program. The program now only prints out raw_input mixed with a string.

---
+++ Pythonlearn.py
@@ -0,0 +1,10 @@
+
+quest = "to aquire the holy grail, no less, no more"
+name = raw_input("what is thy name, you nincompoop?")
+
+
+print "you! %s have been chosen to do your kings quest. What kind of quest you ask?! your quest is merely %s" % (name,quest)
+
+answer = raw_input("do you accept?")
+
+
